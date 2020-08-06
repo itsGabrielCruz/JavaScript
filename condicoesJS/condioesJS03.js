3daf9198fdd279529dd87478468c3a6c0e89f6cd
Gabriel Cruz <itsgabrielcruz@gmail.com>
2020-08-05 21:01:22 -0300

    Práticas de Estudo
    Alguns arquivos que criei durante meus estudos de JavaScript.

---
+++ condicoesJS/condioesJS03.js
@@ -0,0 +1,9 @@
+var idade = 59
+if (idade >= 16 && idade < 18) {
+    console.log('Voto opcional')
+} else if (idade > 60) {
+    console.log('Voto opcional')
+    
+} else {
+    console.log('Voto obrigatório')
+}
